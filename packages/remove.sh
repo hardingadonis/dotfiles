6eadd0eece3e5cc1791f65826d5930b0cb8ecbe5
Minh Vương <hardingadonis@gmail.com>
2025-01-02 23:53:47 +0700

    add: create file `remove.sh`

---
+++ packages/remove.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+
+echo "Remove meld"
+yay -Rsnc meld
+
+echo "Remove kate"
+yay -Rsnc kate
+
+echo "Remove okular"
+yay -Rsnc okular
+
+echo "Remove firefox"
+yay -Rsnc firefox
+
+echo "Remove haruna"
+yay -Rsnc haruna
+
+echo "Remove mpv"
+yay -Rsnc mpv
+
+echo "Remove yad"
+yay -Rsnc yad
+
+echo "Remove kwrited"
+yay -Rsnc kwrited
+
+echo "Remove uxterm"
+yay -Rsnc uxterm
+
+echo "Remove xterm"
+yay -Rsnc xterm
