a054c513fa25f000fcd9dc47306e7da3a811d9cf
Minh Vương <hardingadonis@gmail.com>
2025-01-02 23:56:47 +0700

    update: remove with `--noconfirm` flag

--- packages/remove.sh
+++ packages/remove.sh
@@ -1,31 +1,31 @@
 #/bin/bash
 
 echo "Remove meld"
-yay -Rsnc meld
+yay -Rsnc meld --noconfirm
 
 echo "Remove kate"
-yay -Rsnc kate
+yay -Rsnc kate --noconfirm
 
 echo "Remove okular"
-yay -Rsnc okular
+yay -Rsnc okular --noconfirm
 
 echo "Remove firefox"
-yay -Rsnc firefox
+yay -Rsnc firefox --noconfirm
 
 echo "Remove haruna"
-yay -Rsnc haruna
+yay -Rsnc haruna --noconfirm
 
 echo "Remove mpv"
-yay -Rsnc mpv
+yay -Rsnc mpv --noconfirm
 
 echo "Remove yad"
-yay -Rsnc yad
+yay -Rsnc yad --noconfirm
 
 echo "Remove kwrited"
-yay -Rsnc kwrited
+yay -Rsnc kwrited --noconfirm
 
 echo "Remove uxterm"
-yay -Rsnc uxterm
+yay -Rsnc uxterm --noconfirm
 
 echo "Remove xterm"
-yay -Rsnc xterm
+yay -Rsnc xterm --noconfirm
