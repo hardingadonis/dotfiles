ee76026ee5ef5824c250563f3a134f7091e35ede
Minh Vương <hardingadonis@gmail.com>
2025-01-04 15:36:54 +0700

    update: add installation script for essential GNOME extensions

---
+++ extensions/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Install Clipboard Indicator
+gext install 779
+
+# Install Vitals
+gext install 1460
+
+# Install Dash to Dock
+gext install 307
+
+# Install Desktop Icons NG (DING)
+gext install 2087
+
+# Install Soft Brightness Plus
+gext install 5943
+
+# Install Radio Kayra
+gext install 7649
