c9795ded5094d93c6dfcd33c11740bc86c17fa7c
Minh Vương <hardingadonis@gmail.com>
2025-01-04 15:39:44 +0700

    update: uncomment package removal and installation steps in setup script

--- setup.sh
+++ setup.sh
@@ -5,19 +5,19 @@ echo "Updating the system..."
 yay -Syu --noconfirm
 
 # Remove unnecessary packages
-# echo "Removing unnecessary packages..."
-# chmod +x packages/remove.sh
-# bash ./packages/remove.sh
+echo "Removing unnecessary packages..."
+chmod +x packages/remove.sh
+bash ./packages/remove.sh
 
 # Install necessary packages
-# echo "Installing necessary packages..."
-# chmod +x packages/install.sh
-# bash ./packages/install.sh
+echo "Installing necessary packages..."
+chmod +x packages/install.sh
+bash ./packages/install.sh
 
 # Install GNOME extensions
-# echo "Installing GNOME extensions..."
-# chmod +x extensions/install.sh
-# bash ./extensions/install.sh
+echo "Installing GNOME extensions..."
+chmod +x extensions/install.sh
+bash ./extensions/install.sh
 
 # Configure the system
 echo "Configuring the system..."
@@ -25,8 +25,8 @@ chmod +x config/config.sh
 bash ./config/config.sh
 
 # Confirm the installation
-#echo "Installation complete!"
-#echo "Reboot the system to apply changes."
-#echo "Press any key to reboot..."
-#read -n 1 -s
-#reboot
+echo "Installation complete!"
+echo "Reboot the system to apply changes."
+echo "Press any key to reboot..."
+read -n 1 -s
+reboot
